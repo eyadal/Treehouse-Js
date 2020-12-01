de062aeb8ec45091bc8e6a35c6de44c161b5a33d
eyadal <31968847+eyadal@users.noreply.github.com>
2020-12-01 22:50:19 +0100

    for loop

---
+++ JsLoops/for.js
@@ -0,0 +1,15 @@
+function getRandomNumber(upper) {
+    return Math.floor( Math.random() * upper ) + 1;
+}
+
+
+
+
+for (let i = 0; i < 10; i++ ) {
+    console.log(`The random number is ${getRandomNumber(10)}`)
+}
+
+for (let i = 5; i <= 100; i++) {
+console.log(i)
+}
+
